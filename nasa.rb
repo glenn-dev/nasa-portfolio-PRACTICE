920cc5aba9cc2224042d1fdb8a5bbaf9c81a2972
Glenn <glenn.marcano90@gmail.com>
2020-01-14 16:22:05 -0300

    Final test version for the academy with no optimizations

--- nasa.rb
+++ nasa.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'json'
 require_relative 'lib/helpers'
 
-#Metodo request.
+# Metodo request.
 def request(url, api_key)
     url = URI(url + '&api_key=' + api_key)
     http = Net::HTTP.new(url.host, url.port)
@@ -14,31 +14,22 @@ def request(url, api_key)
     JSON.parse(response.read_body)
 end
 
-#Metodo que toma la respuesta del metodo request, 'limpia' la data y construye un documento html.
+# Metodo que recive la respuesta del request, "limpia" la data y construye un documento html.
 def buid_web_page (respuesta)
 
-    clean_data = respuesta['photos']    #Arreglo que almacena elementos (tipo hash) contenidos dentro del hash principal.
-    photo_info = {}                     #Hash que almacena cada elemento dentro del arreglo 'clean_data'.
-    photo_src = []                      #Arreglo que almacena las url de las fotos provistas por la key 'img_src' dentro del hash 'photo_info'.
-    info_camera = []
-
-    clean_data.each do |i|              #Recorre el contenido para obtener la url de cada foto.
-        photo_info = i
-        photo_src.push(photo_info['img_src']) #Crea un arreglo con las url de cada foto.
-        info_camera.push(photo_info['camera']) #Crea un arreglo con los elementos del hash 'camera'.
-    end
-
-    acum = Hash.new(0)                  #acumulador de elementos del hash 'camera'.
-    info_camera.each do |v|
-        acum[v] += 1
-    end
-    acum.each do |k, v|
-        puts "Hay #{v} fotos de la camara #{k['name']}"
-    end
+    clean_data = respuesta['photos']    # Arreglo que almacena elementos (tipo hash) contenidos dentro del hash principal.
+    photo_info = {}                     # Hash que almacena cada elemento dentro del arreglo 'clean_data'.
+    photo_src = []                      # Arreglo que almacena las url de las fotos provistas por la key 'img_src' dentro del hash 'photo_info'.
     
-    File.open('index.html', 'w') do |f| #Construye el documento html.
+    clean_data.each do |i|              # Recorre el contenido para obtener la url de cada foto.
+        photo_info = i
+        photo_src.push(photo_info['img_src']) # Crea un arreglo con las url de cada foto.
+    end
 
-        f.puts head                     #Llama a metodo que contiene estructura del header.
+    # Construyendo el documento html.
+    File.open('index.html', 'w') do |f|
+
+        f.puts head                     # Llama a metodo que contiene estructura del header.
     
             f.puts '<h1>Portafolio NASA</h1>
                 <ul>'
@@ -50,10 +41,30 @@ def buid_web_page (respuesta)
             f.puts '</ul>'
         end
 
-        f.puts footer                   #Llama a metodo que contiene estructura del footer.
+        f.puts footer                   # Llama a metodo que contiene estructura del footer.
     end
 end
 
+# Metodo que cuenta las fotos de cada camara.
+def photos_count (hash_res)             
+    info = {}          
+    info_camera = []
+    hash_res['photos'].each do |i|
+        info = i
+        info_camera.push(info['camera'])
+    end
+    
+    acum = Hash.new(0)
+    info_camera.each do |v|
+        acum[v] += 1
+    end
+    acum.each do |k, v|
+        puts "Hay #{v} fotos de la camara #{k['name']}"
+    end
+end
 
 #llamado de prueba
-buid_web_page(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10', 'fmU7e9xWOdVaJsBJVZjWtjOkcm6Sbw9id6e8feLi'))
+#photos_count(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10', 'fmU7e9xWOdVaJsBJVZjWtjOkcm6Sbw9id6e8feLi'))
+
+#llamado de prueba
+#buid_web_page(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10', 'fmU7e9xWOdVaJsBJVZjWtjOkcm6Sbw9id6e8feLi'))
